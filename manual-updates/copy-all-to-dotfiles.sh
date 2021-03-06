f2b8271a0d8c1ddd6863416c207487df168a5033
Edwin van Rooij <edwinvrooij@gmail.com>
2021-03-06 12:13:56 +0100

    add new scripts

---
+++ manual-updates/copy-all-to-dotfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+bash ~/dotfiles/manual-updates/copy-power-toys-settings-to-dotfiles.sh
+bash ~/dotfiles/manual-updates/copy-windows-terminal-settings-to-dotfiles.sh
---
+++ manual-updates/copy-all-to-source.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+bash ~/dotfiles/manual-updates/copy-power-toys-settings-to-source.sh
+bash ~/dotfiles/manual-updates/copy-windows-terminal-settings-to-source.sh
