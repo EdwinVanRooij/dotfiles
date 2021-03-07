cd9bada9c5bbf0f7c81dacd50a55e4382db8dd9f
Edwin van Rooij <edwinvrooij@gmail.com>
2021-03-07 11:44:37 +0100

    update powershell

--- config/powershell-profile.ps1
+++ config/powershell-profile.ps1
@@ -1,14 +1,34 @@
+# Easier Navigation: .., ..., ...., ....., and ~
+${function:~} = { Set-Location ~ }
+# PoSh won't allow ${function:..} because of an invalid path error, so...
+${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
+${function:...} = { Set-Location ..\.. }
+${function:....} = { Set-Location ..\..\.. }
+${function:.....} = { Set-Location ..\..\..\.. }
+${function:......} = { Set-Location ..\..\..\..\.. }
+
+# General shortcuts
 New-Alias c clear
-New-Alias e exit
 New-Alias l ls
-
-Function DirectoryUp {cd ..}
-New-Alias .. DirectoryUp
-
-Function DirectoryUp2 {cd ..\..}
-New-Alias ... DirectoryUp2
-
-Function DirectoryUp3 {cd ..\..\..}
-New-Alias .... DirectoryUp3
-
 New-Alias sw winget.exe
+
+function makedir { CreateAndSet-Directory }
+New-Alias mkd makedir
+
+function ex { exit }
+New-Alias e ex
+
+# Navigation Shortcuts
+${function:drop} = { Set-Location ~\Documents\Dropbox }
+${function:dt} = { Set-Location ~\Desktop }
+${function:docs} = { Set-Location ~\Documents }
+${function:dl} = { Set-Location ~\Downloads }
+
+# Empty the Recycle Bin on all drives
+Set-Alias emptytrash Empty-RecycleBin
+
+# Cleanup old files all drives
+Set-Alias cleandisks Clean-Disks
+
+# Reload the shell
+Set-Alias powershell Reload-Powershell
