ce6f2f26b9130c214b08c2612e55b3d69fe91b65
Edwin van Rooij <edwinvrooij@gmail.com>
2021-03-09 21:00:20 +0100

    add install angular, rm powertoys config for now

--- manual-updates/copy-all-to-dotfiles.sh
+++ manual-updates/copy-all-to-dotfiles.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-bash ~/dotfiles/manual-updates/copy-power-toys-settings-to-dotfiles.sh
+# On second thought, this is not a good idea. Manually copying files into there caused bugs, disable it until further notice.
+#bash ~/dotfiles/manual-updates/copy-power-toys-settings-to-dotfiles.sh
 bash ~/dotfiles/manual-updates/copy-windows-terminal-settings-to-dotfiles.sh
 bash ~/dotfiles/manual-updates/copy-powershell-profile-to-dotfiles.sh
--- manual-updates/copy-all-to-source.sh
+++ manual-updates/copy-all-to-source.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-bash ~/dotfiles/manual-updates/copy-power-toys-settings-to-source.sh
+# On second thought, this is not a good idea. Manually copying files into there caused bugs, disable it until further notice.
+#bash ~/dotfiles/manual-updates/copy-power-toys-settings-to-source.sh
 bash ~/dotfiles/manual-updates/copy-windows-terminal-settings-to-source.sh
-bash ~/dotfiles/manual-updates/copy-powershell-profile-to-source.fish
+fish ~/dotfiles/manual-updates/copy-powershell-profile-to-source.fish
 bash ~/dotfiles/manual-updates/copy-vimrc-to-windows-home.sh
