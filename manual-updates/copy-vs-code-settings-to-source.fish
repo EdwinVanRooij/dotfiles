3bcf31ea27abcdc2e2dd581ccbb2dd27da5da6a4
Edwin van Rooij <edwinvrooij@gmail.com>
2021-03-11 23:03:54 +0100

    create vs code vimrc

--- manual-updates/copy-vs-code-settings-to-source.fish
+++ manual-updates/copy-vs-code-settings-to-source.fish
@@ -1,8 +1,14 @@
 #!/usr/bin/fish
 
+# Settings
 cd /mnt/c/Users/*/AppData/Local/Packages
 cd ../../..
+
+# Vimrc
+cp ~/dotfiles/config/vs-code.vimrc vs-code.vimrc
+
 cd AppData/Roaming/Code/User
 cp ~/dotfiles/config/vs-code-settings.json settings.json
 
+# Keybindings
 cp ~/dotfiles/config/vs-code-keybindings.json keybindings.json
