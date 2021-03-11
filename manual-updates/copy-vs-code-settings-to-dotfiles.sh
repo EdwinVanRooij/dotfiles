af83fd94ce15e4f6789864d4257841eeaea56de6
Edwin van Rooij <edwinvrooij@gmail.com>
2021-03-11 22:53:01 +0100

    add vscode keybindings

--- manual-updates/copy-vs-code-settings-to-dotfiles.sh
+++ manual-updates/copy-vs-code-settings-to-dotfiles.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Settings
 cp /mnt/c/Users/*/AppData/Roaming/Code/User/settings.json ~/dotfiles/config/vs-code-settings.json
+
+# Keybindings
+cp /mnt/c/Users/*/AppData/Roaming/Code/User/keybindings.json ~/dotfiles/config/vs-code-keybindings.json
--- manual-updates/copy-vs-code-settings-to-source.fish
+++ manual-updates/copy-vs-code-settings-to-source.fish
@@ -4,3 +4,5 @@ cd /mnt/c/Users/*/AppData/Local/Packages
 cd ../../..
 cd AppData/Roaming/Code/User
 cp ~/dotfiles/config/vs-code-settings.json settings.json
+
+cp ~/dotfiles/config/vs-code-keybindings.json keybindings.json
