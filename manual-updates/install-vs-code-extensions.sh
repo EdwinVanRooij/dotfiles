73500956ccdfee55ec02febabc91ac204afed3ad
Edwin van Rooij <edwinvrooij@gmail.com>
2021-03-12 19:24:42 +0100

    update vs code stuff

--- manual-updates/install-vs-code-extensions.sh
+++ manual-updates/install-vs-code-extensions.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
 code --install-extension eamodio.gitlens
+code --install-extension bmalehorn.vscode-fish
+code --install-extension christian-kohler.path-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension PKief.material-icon-theme
 code --install-extension slevesque.vscode-autohotkey
+code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscodevim.vim
+code --install-extension xabikos.JavaScriptSnippets
