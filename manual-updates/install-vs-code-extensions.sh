56ac226575f6143863d7d1c273631bacf9427d60
Edwin van Rooij <edwinvrooij@gmail.com>
2021-03-13 22:18:19 +0100

    init new extension method

--- manual-updates/install-vs-code-extensions.sh
+++ manual-updates/install-vs-code-extensions.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-code --install-extension eamodio.gitlens
-code --install-extension bmalehorn.vscode-fish
-code --install-extension christian-kohler.path-intellisense
-code --install-extension CoenraadS.bracket-pair-colorizer
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension esbenp.prettier-vscode
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension PKief.material-icon-theme
-code --install-extension slevesque.vscode-autohotkey
-code --install-extension VisualStudioExptTeam.vscodeintellicode
-code --install-extension vscodevim.vim
-code --install-extension xabikos.JavaScriptSnippets
-code --install-extension ms-vscode.powershell
+cat ~/dotfiles/config/vs-code-extensions.txt | while read extension || [[ -n $extension ]];
+do
+	  code --install-extension $extension
+	  #code --install-extension $extension --force
+done
